52bbf344e7ce23cc798ba319f351c883d0a658b3
LichuDev <lujanlisandro.11@gmail.com>
2025-01-24 17:05:13 -0300

    Integrando JWT

--- src/main/java/com/lichu/Backend_Canchas/Config/SecurityConfig.java
+++ src/main/java/com/lichu/Backend_Canchas/Config/SecurityConfig.java
@@ -6,22 +6,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Deshabilita CSRF si no lo necesitas
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register","/login").permitAll() // Permite el acceso sin autenticación
-                        .anyRequest().authenticated() // Requiere autenticación para el resto
-                );
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .build();
+
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
