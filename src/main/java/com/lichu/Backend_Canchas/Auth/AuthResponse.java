c5d3e8cbc89acd37b7a8d80027c51b55a8981571
LichuDev <lujanlisandro.11@gmail.com>
2025-01-24 17:05:21 -0300

    Integrando JWT

---
+++ src/main/java/com/lichu/Backend_Canchas/Auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.lichu.Backend_Canchas.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    String token;
+}
--- src/main/java/com/lichu/Backend_Canchas/Controller/UserController.java
+++ src/main/java/com/lichu/Backend_Canchas/Controller/UserController.java
@@ -1,6 +1,5 @@
 package com.lichu.Backend_Canchas.Controller;
 
-
 import com.lichu.Backend_Canchas.Model.User;
 import com.lichu.Backend_Canchas.Service.UserService;
 import jakarta.validation.Valid;
@@ -9,25 +8,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth")
 public class UserController {
     @Autowired
     private UserService userServ;
 
     @CrossOrigin(origins = "http://localhost:3000") // Cambia a la URL de tu frontend
     @PostMapping("/register")
-    public ResponseEntity<?> registeUser(@Valid @RequestBody User usuario){
+    public ResponseEntity<?> registeUser(@Valid @RequestBody User usuario) {
         userServ.registerUser(usuario);
         return ResponseEntity.ok("Usuario creado con exito");
 
     }
+
     @CrossOrigin(origins = "http://localhost:3000") // Cambia a la URL de tu frontend
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@Valid @RequestBody User usuario){
+    public ResponseEntity<?> loginUser(@Valid @RequestBody User usuario) {
         Boolean auth = userServ.checkLogin(usuario);
-        if (auth){
+        if (auth) {
             System.out.println("Autenticado");
             return ResponseEntity.ok("Se inicio sesion correctamente");
-        }else {
+        } else {
             return ResponseEntity.status(401).body("El mail y la contraseña no coincide");
         }
 
